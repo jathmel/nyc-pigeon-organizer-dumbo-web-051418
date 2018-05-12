f9752e93f5184a874ef8c0dd480a429d1069e8b4
Jathmel Gordon <jathmel@gmail.com>
2018-05-12 21:17:19 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  pigeon_name = []
+  data.
   # write your code here!
+end
+
+def pigeon_name(hash)
+  result = []
+  hash.each do |key, value|
+    result += value
+  end
+  result.uniq
 end
